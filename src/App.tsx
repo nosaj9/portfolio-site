7bf906848b10652a4cb2fc9529aa1382ca373671
BuildTools <jdcomman27@gmail.com>
2023-01-05 11:27:31 -0500

    changed background color

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,6 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
-import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
+import Navbar from "./components/Navbar";
 import Main from "./Main";
 
 function App() {
