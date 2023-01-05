1d814a34ace69bc2d3489fa472b2980ba119056d
BuildTools <jdcomman27@gmail.com>
2023-01-05 17:36:31 -0500

    added page style

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -2,13 +2,19 @@ import "./Main.scss";
 import { Routes, Route } from 'react-router-dom';
 import Home from '../pages/Home';
 import About from '../pages/About';
+import Projects from '../pages/Projects';
+import Acappella from '../pages/Acappella';
 
 const Main = () => {
     return (
-        <Routes> {/* The Switch decides which component to show based on the current URL.*/}
-            <Route path="/" element={<Home />} />
-            <Route path="/about" element={<About />} />
-        </Routes>
+        <div className="Main">
+            <Routes> {/* The Switch decides which component to show based on the current URL.*/}
+                <Route path="/" element={<Home />} />
+                <Route path="/about" element={<About />} />
+                <Route path="/projects" element={<Projects />} />
+                <Route path="/acappella" element={<Acappella />} />
+            </Routes>
+        </div>
     );
 }
 
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,21 +2,25 @@ import { NavLink } from "react-router-dom";
 import "./Navbar.scss"
 
 export default function Navbar() {
-    const activeLink = "bg-blue-100 text-black";
-    const normalLink = "";
+    const activeLink = "active";
+    const normalLink = "normal";
 
     return (
         <nav className="navbar">
             <div className="option">
-                <NavLink to="/" className={({ isActive }) => (isActive ? activeLink : normalLink)}>
-                Home
-                </NavLink>
+                <NavLink to="/" className={({ isActive }) => (isActive ? activeLink : normalLink)}>Home</NavLink>
             </div>
 
             <div className="option">
-                <NavLink to="/about" className={({ isActive }) => (isActive ? activeLink : normalLink)}>
-                About
-                </NavLink>
+                <NavLink to="/about" className={({ isActive }) => (isActive ? activeLink : normalLink)}>About</NavLink>
+            </div>
+
+            <div className="option">
+                <NavLink to="/projects" className={({ isActive }) => (isActive ? activeLink : normalLink)}>Projects</NavLink>
+            </div>
+
+            <div className="option">
+                <NavLink to="/acappella" className={({ isActive }) => (isActive ? activeLink : normalLink)}>Acappella</NavLink>
             </div>
         </nav>
     )
