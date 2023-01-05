7dd86ce504652672d0e72913537646f3922b6a7d
BuildTools <jdcomman27@gmail.com>
2023-01-05 01:17:28 -0500

    changed background color

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -11,17 +11,19 @@ import "./Footer.css"
 export default function Footer() {
     return (
         <footer>
-            <a href="https://github.com/nosaj9/">
-              <IoLogoGithub />
-            </a>
+            <div className="icons">
+                <a href="https://github.com/nosaj9/">
+                <IoLogoGithub />
+                </a>
 
-            <a href="https://www.linkedin.com/in/jason-d-wang/">
-              <IoLogoLinkedin />
-            </a>
+                <a href="https://www.linkedin.com/in/jason-d-wang/">
+                <IoLogoLinkedin />
+                </a>
 
-            <a href="mailto:jwangd8@gmail.com">
-              <IoMail />
-            </a>
+                <a href="mailto:jwangd8@gmail.com">
+                <IoMail />
+                </a>
+            </div>
 
             <p>Copyright © 2023 — All Rights Reserved </p>
         </footer>
