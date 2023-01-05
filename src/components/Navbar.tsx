a2e7fe180afeee592d48f8c136ea7faab3c832ac
BuildTools <jdcomman27@gmail.com>
2023-01-05 17:06:09 -0500

    added navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,13 +1,23 @@
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 import "./Navbar.scss"
 
 export default function Navbar() {
+    const activeLink = "bg-blue-100 text-black";
+    const normalLink = "";
+
     return (
-        <nav>
-            <h1>Navbar component</h1>
-            <Link to="/about">
-            <button>About</button>
-            </Link>
+        <nav className="navbar">
+            <div className="option">
+                <NavLink to="/" className={({ isActive }) => (isActive ? activeLink : normalLink)}>
+                Home
+                </NavLink>
+            </div>
+
+            <div className="option">
+                <NavLink to="/about" className={({ isActive }) => (isActive ? activeLink : normalLink)}>
+                About
+                </NavLink>
+            </div>
         </nav>
     )
 }
