3779e02b8867df110a0a42ac11b63b74385aa297
BuildTools <jdcomman27@gmail.com>
2023-01-05 01:07:34 -0500

    added some components

---
+++ src/components/Footer.tsx
@@ -0,0 +1,29 @@
+import React from "react"
+
+import {
+    IoLogoGithub,
+    IoLogoLinkedin,
+    IoMail,
+} from "react-icons/io5"
+
+import "./Footer.css"
+
+export default function Footer() {
+    return (
+        <footer>
+            <a href="https://github.com/nosaj9/">
+              <IoLogoGithub />
+            </a>
+
+            <a href="https://www.linkedin.com/in/jason-d-wang/">
+              <IoLogoLinkedin />
+            </a>
+
+            <a href="mailto:jwangd8@gmail.com">
+              <IoMail />
+            </a>
+
+            <p>Copyright © 2023 — All Rights Reserved </p>
+        </footer>
+    )
+}
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,11 @@
+import React from "react"
+
+import "./Footer.css"
+
+export default function Navbar() {
+    return (
+        <nav>
+            <h1>Navbar component</h1>
+        </nav>
+    )
+}
