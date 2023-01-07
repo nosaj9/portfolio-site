fc8fa6ecc921e43f20f2ae1b584b8e634a30d79b
BuildTools <jdcomman27@gmail.com>
2023-01-06 22:53:44 -0500

    added about page transitions

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -7,7 +7,7 @@ const About = () => {
             <h2>A little bit about me..</h2>
             <p>
                 Hi, I'm Jason! I'm a <span className="highlight">computer science</span> major studying at the University of Maryland, College Park where I'm also currently part
-                of the Advanced Cybersecurity Experience for Students (ACES) Program. I'm also part of an <span className="highlight">acappella group</span> called Dacadence, more on that under the <NavLink to="/acappella" className="pagelink">Acappella</NavLink> tab.
+                of the Advanced Cybersecurity Experience for Students (ACES) Program. 
                 After graduating, I hope to be a <span className="highlight">software engineer</span>!
             </p>
 
