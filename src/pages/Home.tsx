5b7cd3a26d016f0ed5f7f6e4b8ec427693d4d15c
BuildTools <jdcomman27@gmail.com>
2023-01-08 18:08:03 -0500

    edited home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -6,8 +6,8 @@ const Home = () => {
         <div className="Home">
             <img src={require("../files/headshot.jpg")} alt="me"/>
             <h1>Hi, I'm Jason!</h1>
-            <h2>A student, singer, and aspiring software engineer.</h2>
-            <p>You have reached my website. Feel free to look around and contact me below!</p>
+            <h2>I'm a student, singer, and aspiring software engineer.</h2>
+            <p>Feel free to look around and <a href="mailto:jwangd8@gmail.com" target="_blank" rel="noreferrer">contact me!</a></p>
         </div>
     )
 }
