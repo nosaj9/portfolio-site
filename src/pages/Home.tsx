258b2862b3c74919664e4ee0ee78c30b4ba15270
BuildTools <jdcomman27@gmail.com>
2023-01-08 18:40:24 -0500

    added navbar color

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -4,10 +4,13 @@ import "./style.scss";
 const Home = () => {
     return (
         <div className="Home">
+            <br/>
             <img src={require("../files/headshot.jpg")} alt="me"/>
             <h1>Hi, I'm Jason!</h1>
             <h2>I'm a student, singer, and aspiring software engineer.</h2>
-            <p>Feel free to look around and <a href="mailto:jwangd8@gmail.com" target="_blank" rel="noreferrer">contact me!</a></p>
+            <p>Feel free to look around and contact me <a href="mailto:jwangd8@gmail.com" target="_blank" rel="noreferrer">here</a>!</p>
+
+            
         </div>
     )
 }
