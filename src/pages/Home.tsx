c507653df054ed8269da948c438c96d2399fe56d
BuildTools <jdcomman27@gmail.com>
2023-01-08 22:32:50 -0500

    added to homepage

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -6,15 +6,24 @@ const Home = () => {
     return (
         <div className="Home">
             <br/>
-            <NavLink to="/about"><img className="me" src={require("../files/headshot.jpg")} alt="me"/></NavLink>
-            <h1>Hi, I'm Jason!</h1>
-            <h2>I'm a student, singer, and aspiring software engineer.</h2>
-            <p>
-                Feel free to look around and contact me <a href="mailto:jwangd8@gmail.com" target="_blank" rel="noreferrer">here</a>!
-                <br/><span className="highlight">Quick Links:</span>
-            </p>
-            {/* <p><span className="highlight">Quick Links:</span></p> */}
-            <NavLink to="/projects"><img className="pokemon" src={require("../files/pkfanclub.png")} alt="pokemon fan club"/></NavLink>
+            <div className="introcontainer">
+                <div>
+                    <h1>Hi, I'm Jason!</h1>
+                    <h2>I'm a student, singer, and aspiring software engineer.</h2>
+                    <p>
+                        Feel free to look around and contact me <a href="mailto:jwangd8@gmail.com" target="_blank" rel="noreferrer">here</a>!
+                        <br/><span className="highlight">Quick Links:</span>
+                    </p>
+                </div>
+                <div>
+                    <NavLink to="/about"><img className="me" src={require("../files/headshot.jpg")} alt="me"/></NavLink>
+                </div>
+            </div>
+
+            <div className="imagecontainer">
+                <NavLink to="/projects"><img className="pokemon" src={require("../files/pkfanclub.png")} alt="pokemon fan club"/></NavLink>
+                <NavLink to="acappella"><img className="dacadence" src={require("../files/dafam.jpg")} alt="dacadence"/></NavLink>
+            </div>
         </div>
     )
 }
